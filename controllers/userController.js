2faac5f7b738f99f0f2d89d5bc8935a7ae5ece2a
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-11 16:02:05 +0900

    feat: Control login and make User Profile page
    login (fake)

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,11 +16,16 @@ export const postJoin = (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
 export const logout = (req, res) => res.send("Logout");
 export const user = (req, res) => res.send("User");
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
-export const userDetail = (req, res) => res.send("User Detail");
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
