23e3a2c07e0cff9afd4f0f5c89c9c55fcfc0b43c
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-11 15:04:48 +0900

    feat: Make fake videos db and List about videos

---
+++ db.js
@@ -0,0 +1,52 @@
+export const videos = [
+  {
+    id: 149081,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1245456,
+      name: "SKH",
+      email: "hello@world.com",
+    },
+  },
+  {
+    id: 52777,
+    title: "Video Good",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1245456,
+      name: "SKH",
+      email: "hello@world.com",
+    },
+  },
+
+  {
+    id: 124355,
+    title: "Video Hello World",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1245456,
+      name: "SKH",
+      email: "hello@world.com",
+    },
+  },
+
+  {
+    id: 90802,
+    title: "Video Nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1245456,
+      name: "SKH",
+      email: "hello@world.com",
+    },
+  },
+];
